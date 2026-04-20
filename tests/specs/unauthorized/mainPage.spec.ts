a737aed2b14f9dd3a906ec7679c5ad182aac6676
Alex <antistov008@gmail.com>
2026-04-20 14:22:30 +0300

    project structure

---
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -0,0 +1,7 @@
+import { Page, test } from '@playwright/test'
+import { MainPage } from '../../pages/MainPage'
+
+test('Открытие главной страницы', async ({ page }) => {
+    const mainPage = new MainPage(page)
+    await mainPage.open()
+})
